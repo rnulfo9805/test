031c6a88c1d2a88dc072acfba4f64b3e08c9d296
YONEEET0901 <sweetbrother0901@gmail.com>
2025-11-10 19:34:26 +0800

    Player.java

--- GAMENIROXAS/src/main/java/entity/Player.java
+++ GAMENIROXAS/src/main/java/entity/Player.java
@@ -1,11 +1,12 @@
 package entity;
 
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import javax.imageio.ImageIO;
-import main.main.GamePanel;
-import main.main.KeyHandler;
+import main.GamePanel;
+import main.KeyHandler;
 
 public class Player extends Entity {
     
@@ -14,7 +15,8 @@ public class Player extends Entity {
     
     public int screenX;
     public int screenY;
-    //start, eto simula ronald, tanggalin mo nlang toh pagtapos mo
+    int counter2 = 0;
+    
     BufferedImage standingLeft, standingRight;
     BufferedImage left1, left2, right1, right2;
     BufferedImage inBetweenLeft, inBetweenRight;
@@ -26,6 +28,14 @@ public class Player extends Entity {
     	
     	screenX = gp.screenWidth/2 - (gp.tileSize/2);
     	screenY = gp.screenHeight/2 - (gp.tileSize/2);
+    	
+    	solidArea = new Rectangle();
+    	solidArea.x = 8;
+    	solidArea.y = 16;
+    	solidArea.width = 32;
+    	solidArea.height = 32;
+    	
+    	
     	
     	setDefaultValues();
 		GetPlayerImage();
@@ -63,18 +73,37 @@ public class Player extends Entity {
       
 	  if (keyH.upPressed || keyH.downPressed || keyH.leftPressed || keyH.rightPressed) {
 		    if (keyH.upPressed) {
-		        direction = "up";
-		        worldY -= speed;
+		        direction = "up";	       
 		    } else if (keyH.downPressed) {
-		        direction = "down";
-		        worldY += speed;
+		        direction = "down"; 
 		    } else if (keyH.leftPressed) {
-		        direction = "left";
-		        worldX -= speed;
+		        direction = "left";	     
 		    } else if (keyH.rightPressed) {
-		        direction = "right";
-		        worldX += speed;
+		        direction = "right";	      
 		    }
+		    
+		    //check tile collision
+		    if (collisionOn == false) {
+		    	switch (direction) {
+		    	case "up":
+		    		worldY -= speed;
+		    	break;
+		    	case "down":
+		    		worldY += speed;
+		    	break;
+		    	case "left":
+		    		 worldX -= speed;
+		    	break;
+		    	case "right":
+		    		 worldX += speed;
+		    	break;
+		    	
+		    	
+		    	}
+		    }
+		    
+		    collisionOn = false;
+		    gp.cChecker.checkTile(this);
 
 		    spriteCounter++;
 		    if (spriteCounter > 6) {
@@ -117,4 +146,3 @@ public class Player extends Entity {
 	    g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
   	}
 }
-		//hanggang dito, copy paste mo nayan bitchass nigga, also remove this pagtapos mo.
