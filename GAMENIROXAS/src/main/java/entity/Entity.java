00f1e160f0db6d88717182da9b07c0f84b347219
YONEEET0901 <sweetbrother0901@gmail.com>
2025-11-10 19:34:50 +0800

    Entity.java

--- GAMENIROXAS/src/main/java/entity/Entity.java
+++ GAMENIROXAS/src/main/java/entity/Entity.java
@@ -1,4 +1,5 @@
 package entity;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 
 public class Entity {
@@ -14,4 +15,8 @@ public class Entity {
     
     public int spriteCounter = 0;
     public int spriteNum = 1;
+    public Rectangle solidArea;
+    public boolean collisionOn=false;
+    
+    
 }
