5abf73fe85ae305b3cb2ef8df8e46673fa74e648
rnulfo9805 <iamedrin@gmail.com>
2025-11-11 15:43:41 +0800

    Update Entity.java

--- GAMENIROXAS/src/main/java/entity/Entity.java
+++ GAMENIROXAS/src/main/java/entity/Entity.java
@@ -6,17 +6,17 @@ public class Entity {
     public int worldX, worldY;
     public int speed;
     
-    public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
-    public BufferedImage up3, up4, down3, down4, left3, left4, right3, right4;
-    public BufferedImage up5, down5, left5, right5;
-    public BufferedImage downStill, upStill, leftStill, rightStill;
+    public BufferedImage up1,up2;
+    public BufferedImage down1,down2;
+    public Rectangle solidArea;
+    public boolean collisionOn = false;
+    BufferedImage standingLeft, standingRight;
+    BufferedImage left1, left2, right1, right2;
+    BufferedImage inBetweenLeft, inBetweenRight;
+   
 
     public String direction; 
     
     public int spriteCounter = 0;
     public int spriteNum = 1;
-    public Rectangle solidArea;
-    public boolean collisionOn=false;
-    
-    
 }
