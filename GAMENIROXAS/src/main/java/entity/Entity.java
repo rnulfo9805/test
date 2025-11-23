248cdb42f2159cc080e5d8cf245d3a1df07966fd
rnulfo9805 <iamedrin@gmail.com>
2025-11-23 15:09:01 +0800

    Add files via upload

---
+++ GAMENIROXAS/src/main/java/entity/Entity.java
@@ -0,0 +1,23 @@
+package entity;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+
+public class Entity {
+    public int worldX, worldY;
+    public int speed;
+    
+    public BufferedImage up1,up2;
+    public BufferedImage down1,down2;
+    public Rectangle solidArea;
+    public boolean collisionOn = false;
+    BufferedImage standingLeft, standingRight;
+    BufferedImage left1, left2, right1, right2;
+    BufferedImage inBetweenLeft, inBetweenRight;
+    BufferedImage inBetweenDown, inBetweenUp;
+   
+
+    public String direction; 
+    
+    public int spriteCounter = 0;
+    public int spriteNum = 1;
+}
